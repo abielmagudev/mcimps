b060f3b5bde785c69b9a929a0ab02d9acb4d5fac
Abiel Magu <abielmagu.dev@gmail.com>
2026-02-16 23:23:28 -0600

    redisena e implementa actualizaciones de show de Cliente

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -3,12 +3,12 @@
 <div class="row">
     <div class="col-lg col-lg-3">
         <x-card>
-            <div class="text-end mb-3">
-                <a href="{{ route('clientes.edit', $cliente->id) }}" class="link-primary">Editar</a>
-            </div>
             <div>
                 <span class="fw-bold">{{ $cliente->nombre_completo }}</span><br>
                 <span>{{ $cliente->telefono }}</span>
+            </div>
+            <div class="text-end mb-3">
+                <a href="{{ route('clientes.edit', $cliente->id) }}" class="link-primary">Editar</a>
             </div>
         </x-card>
     </div>
@@ -27,30 +27,31 @@
             <x-table>
                 <x-slot name="thead">
                     <tr>
-                        <th>Calle</th>
-                        <th>Colonia</th>
-                        <th class="text-nowrap">Código Postal</th>
-                        <th>Ciudad</th>
-                        <th>Estado</th>
-                        <th>Contacto</th>
-                        <th>Teléfono</th>
+                        <th>Información</th>
                         <th>Cobertura</th>
-                        <th>Referencias</th>
+                        <th>Contacto</th>
                         <th></th>
                     </tr>
                 </x-slot>
 
                 @foreach ($cliente->direcciones->reverse() as $direccion)
-                <tr class="small">
-                    <td>{{ $direccion->calle }}</td>
-                    <td>{{ $direccion->colonia }}</td>
-                    <td>{{ $direccion->codigo_postal }}</td>
-                    <td>{{ $direccion->ciudad }}</td>
-                    <td>{{ $direccion->estado }}</td>
-                    <td>{{ $direccion->nombre_contacto }}</td>
-                    <td>{{ $direccion->telefono_contacto }}</td>
+                <tr>
+                    <td class="text-nowrap">
+                        <span class="d-block">{{ $direccion->lineal }}</span>
+                        @isset($direccion->referencias)                  
+                        <small class="text-secondary">Referencias: {{ $direccion->referencias }}</small>
+                        @endisset
+                    </td>
                     <td class="text-capitalize">{{ $direccion->cobertura }}</td>
-                    <td>{{ $direccion->referencias }}</td>
+                    <td>
+                        @isset($direccion->nombre_contacto)
+                        <span class="d-block">{{ $direccion->nombre_contacto }}</span>
+                        @endisset
+
+                        @isset($direccion->telefono_contacto)
+                        <span>{{ $direccion->telefono_contacto }}</span>
+                        @endisset
+                    </td>
                     <td class="text-end">
                         <a href="{{ route('clientes.direcciones.edit', [$cliente, $direccion]) }}" class="link-primary">Editar</a>
                     </td>
