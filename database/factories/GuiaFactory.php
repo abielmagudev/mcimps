99cc93e039a2f75a452abc4cf58db62af1633e76
Abiel Magu <abielmagu.dev@gmail.com>
2026-02-25 16:35:22 -0600

    refactoriza logica de factory y seeder para entender proceso de las Guias

--- database/factories/GuiaFactory.php
+++ database/factories/GuiaFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Guia\GuiaStatusEnum;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,14 +16,18 @@ class GuiaFactory extends Factory
      */
     public function definition(): array
     {
+        $numero_rastreo_mex = $this->faker->optional()->numberBetween(1000, 9999);
+        $registro_salida = $numero_rastreo_mex ? $this->faker->optional()->numberBetween(1000, 9999) : null;
+        $fecha_salida = $registro_salida ? $this->faker->dateTime() : null;
+
         return [
             'numero_rastreo_origen' => $this->faker->optional()->numberBetween(1000, 9999),
             'numero_rastreo_usa' => $this->faker->numberBetween(1000, 9999),
-            'numero_rastreo_mex' => $this->faker->optional()->numberBetween(1000, 9999),
-            'registro_salida' => $this->faker->optional()->numberBetween(1000, 9999),
-            'fecha_salida' => $this->faker->optional()->dateTime(),
+            'numero_rastreo_mex' => $numero_rastreo_mex,
+            'registro_salida' => $registro_salida,
+            'fecha_salida' => $fecha_salida,
             'observaciones' => $this->faker->optional()->sentence(),
-            'status' => $this->faker->randomElement(GuiaStatusEnum::cases()),
+            // 'status' => $this->faker->randomElement(GuiaStatusEnum::cases()),
             // 'direccion_id' => $this->faker->randomElement(factory(App\Models\Direccion::class)->count()),
             // 'transportadora_id' => $this->faker->randomElement(factory(App\Models\Transportadora::class)->count()),
             // 'salida_por_usuario' => $this->faker->randomElement([1, 2]),
--- database/seeders/GuiaSeeder.php
+++ database/seeders/GuiaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Direccion;
 use App\Models\Guia;
+use App\Models\Guia\GuiaStatusEnum;
 use App\Models\Transportadora;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,13 +18,32 @@ class GuiaSeeder extends Seeder
     {
         $direcciones = Direccion::all();
         $transportadoras = Transportadora::all();
-
         $guias = Guia::factory(100)->make();
 
-        foreach ($guias as $guia) {
-            $guia->direccion_id = mt_rand(0,1) ? $direcciones->random()->id : null;
-            $guia->transportadora_id = mt_rand(0,1) ? $transportadoras->random()->id : null;
-            $guia->salida_por_usuario = $guia->fecha_salida ? mt_rand(1,10) : null;
+        foreach ($guias as $guia)
+        {
+            if( $guia->numero_rastreo_mex )
+            {
+                $guia->direccion_id = $direcciones->random()->id;
+                $guia->transportadora_id = $transportadoras->random()->id;
+            }
+            else
+            {
+                $guia->direccion_id = mt_rand(0,1) ? $direcciones->random()->id : null;
+                $guia->transportadora_id = $guia->direccion_id && mt_rand(0,1) ? $transportadoras->random()->id : null;
+            }
+
+            if( $guia->transportadora_id &&! $guia->registro_salida )
+            {
+                $guia->status = GuiaStatusEnum::PENDIENTE;
+            }
+
+            if( $guia->registro_salida )
+            {
+                $guia->salida_por_usuario = mt_rand(1,10);
+                $guia->status = mt_rand(0,1) ? GuiaStatusEnum::ENTREGADO : GuiaStatusEnum::TRANSITO;
+            }
+
             $guia->creado_por_usuario = mt_rand(1,10);
             $guia->actualizado_por_usuario = mt_rand(1,10);
             $guia->save();
