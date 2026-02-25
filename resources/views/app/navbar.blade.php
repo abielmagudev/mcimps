c7400cea5a4b1b113f2965f2c402e782d56dcdc5
Abiel Magu <abielmagu.dev@gmail.com>
2026-02-25 00:39:44 -0600

    agrega acento a palabra Guias en navbar

--- resources/views/app/navbar.blade.php
+++ resources/views/app/navbar.blade.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link {{ request()->routeIs('guias.*') ? 'active' : '' }}" aria-current="page" href="{{ route('guias.index') }}">Guias</a>
+          <a class="nav-link {{ request()->routeIs('guias.*') ? 'active' : '' }}" aria-current="page" href="{{ route('guias.index') }}">Guías</a>
         </li>
         <li class="nav-item">
           <a class="nav-link {{ request()->routeIs('clientes.*') ? 'active' : '' }}" href="{{ route('clientes.index') }}">Clientes</a>
