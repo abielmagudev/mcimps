550fa483d6af04266099f8b1573af2010e2933ec
Abiel Magu <abielmagu.dev@gmail.com>
2026-03-01 04:20:16 -0600

    actualiza subcomponente de direccion

--- resources/views/direcciones/inc/direccion-completa-horizontal.blade.php
+++ resources/views/direcciones/inc/direccion-completa-horizontal.blade.php
@@ -1,12 +1,11 @@
 {{ $direccion->calle }},
 {{ $direccion->colonia }},
 {{ $direccion->ciudad }},
-{{ $direccion->estado }} 
+{{ $direccion->estado }}, 
 C.P. {{ $direccion->codigo_postal }}
 
 @isset($direccion->referencias) 
-<div>
-    <small class="text-secondary">Referencias:</small> 
-    <small>{{ $direccion->referencias }}</small>
-</div>
+<br>
+<small class="text-secondary">Referencias:</small> 
+<small>{{ $direccion->referencias }}</small>
 @endisset   
