a2fdb7a8e226d1aa432ca6714e010cf0f0357e7e
Abiel Magu <abielmagu.dev@gmail.com>
2026-03-01 07:04:29 -0600

    crea observers de transportadora y user

---
+++ app/Observers/TransportadoraObserver.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Transportadora;
+
+class TransportadoraObserver
+{
+    /**
+     * Handle the Transportadora "created" event.
+     */
+    public function created(Transportadora $transportadora): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Transportadora "updated" event.
+     */
+    public function updated(Transportadora $transportadora): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Transportadora "deleted" event.
+     */
+    public function deleted(Transportadora $transportadora): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Transportadora "restored" event.
+     */
+    public function restored(Transportadora $transportadora): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Transportadora "force deleted" event.
+     */
+    public function forceDeleted(Transportadora $transportadora): void
+    {
+        //
+    }
+}
