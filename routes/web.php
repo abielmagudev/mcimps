4afcef92b1c309dc855533a2b7119f0ef30fd4e4
Abiel Magu <abielmagu.dev@gmail.com>
2026-03-02 23:27:06 -0600

    codifica redireccion a guias en case de esta vacio la url

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('guias.index');
 });
 
 Route::resource('clientes', \App\Http\Controllers\ClienteController::class);
