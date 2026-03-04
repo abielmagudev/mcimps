9e05550760afa571a5e2be62b9695cedfe28332f
Abiel Magu <abielmagu.dev@gmail.com>
2026-03-04 01:53:39 -0600

    actualiza diseno de impresion de etiqueta de guia

--- resources/views/guias/imprimir.blade.php
+++ resources/views/guias/imprimir.blade.php
@@ -1,45 +1,47 @@
 @extends('print')
 @section('content')
-<table class="table border-light mb-3">
-    <tbody>
-        <tr>
-            <td class="bg-body-secondary" style="width:1%">Rastreo USA</td>
-            <td>{{ $guia->numero_rastreo_usa }}</td>
-        </tr>
-        <tr>
-            <td class="bg-body-secondary">Transportadora</td>
-            <td>{{ $guia->transportadora?->nombre }}</td>
-        </tr>
-        <tr>
-            <td class="bg-body-secondary">Nombre</td>
-            <td>{{ $guia->nombre_contacto }}</td>
-        </tr>
-        <tr>
-            <td class="bg-body-secondary">Télefono</td>
-            <td>{{ $guia->telefono_contacto }}</td>
-        </tr>
-        <tr>
-            <td class="bg-body-secondary">Dirección</td>
-            <td>{{ $guia->direccion?->calle }}, {{ $guia->direccion?->colonia }}</td>
-        </tr>
-        <tr>
-            <td class="bg-body-secondary">Referencias</td>
-            <td>{{ $guia->direccion?->referencias }}</td>
-        </tr>
-        <tr>
-            <td class="bg-body-secondary">Ciudad</td>
-            <td>{{ $guia->direccion?->ciudad }}</td>
-        </tr>
-        <tr>
-            <td class="bg-body-secondary">Estado</td>
-            <td>{{ $guia->direccion?->estado }}</td>
-        </tr>
-        <tr>
-            <td class="bg-body-secondary">Cobertura</td>
-            <td>{{ ucfirst($guia->direccion?->cobertura) }}</td>
-        </tr>
-    </tbody>
-</table>
+<div class="mb-3">
+    <table class="table border-light mb-3">
+        <tbody>
+            <tr>
+                <td class="bg-body-secondary" style="width:1%">Rastreo USA</td>
+                <td>{{ $guia->numero_rastreo_usa }}</td>
+            </tr>
+            <tr>
+                <td class="bg-body-secondary">Transportadora</td>
+                <td>{{ $guia->transportadora?->nombre }}</td>
+            </tr>
+            <tr>
+                <td class="bg-body-secondary">Nombre</td>
+                <td>{{ $guia->nombre_contacto }}</td>
+            </tr>
+            <tr>
+                <td class="bg-body-secondary">Télefono</td>
+                <td>{{ $guia->telefono_contacto }}</td>
+            </tr>
+            <tr>
+                <td class="bg-body-secondary">Dirección</td>
+                <td>{{ $guia->direccion?->calle }}, {{ $guia->direccion?->colonia }}</td>
+            </tr>
+            <tr>
+                <td class="bg-body-secondary">Referencias</td>
+                <td>{{ $guia->direccion?->referencias }}</td>
+            </tr>
+            <tr>
+                <td class="bg-body-secondary">Ciudad</td>
+                <td>{{ $guia->direccion?->ciudad }}</td>
+            </tr>
+            <tr>
+                <td class="bg-body-secondary">Estado</td>
+                <td>{{ $guia->direccion?->estado }}</td>
+            </tr>
+            <tr>
+                <td class="bg-body-secondary">Cobertura</td>
+                <td>{{ ucfirst($guia->direccion?->cobertura) }}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 <div class="text-center">
     {!! $barcode !!}
 </div>
