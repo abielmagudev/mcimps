46a253ff65868b9cc2768f58fab6fb67d7293099
Abiel Magu <abielmagu.dev@gmail.com>
2026-03-04 22:58:32 -0600

    ordena guias index de antiguo a nuevo por updated_at

--- app/Http/Controllers/GuiaController.php
+++ app/Http/Controllers/GuiaController.php
@@ -41,7 +41,7 @@ class GuiaController extends Controller
             $guiasQuery = $guiasQuery->where('status', GuiaStatusEnum::RECIBIDO);
         }
 
-        $guias = $guiasQuery->paginate(100);
+        $guias = $guiasQuery->orderBy('updated_at', 'ASC')->paginate(100);
         // dd( get_class_methods($guias) );
 
         $contadores = [
