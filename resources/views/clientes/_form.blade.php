68ff6d0ec211a235886f91462c5d9ff67f7c4de3
Abiel Magu <abielmagu.dev@gmail.com>
2026-03-05 01:33:27 -0600

    agrega validaciones en form de clientes

--- resources/views/clientes/_form.blade.php
+++ resources/views/clientes/_form.blade.php
@@ -1,8 +1,10 @@
 <div class="mb-3">
   <label for="nombreCompletoInput" class="form-label">Nombre completo</label>
-  <input type="text" class="form-control" id="nombreCompletoInput" name="nombre_completo" value="{{ $cliente->nombre_completo }}" autofocus required>
+  <input type="text" class="form-control {{ bsIsInvalidClass('nombre_completo') }}" id="nombreCompletoInput" name="nombre_completo" value="{{ old('nombre_completo', $cliente->nombre_completo) }}" autofocus required>
+  <x-invalid-feedback name="nombre_completo" />
 </div>
 <div class="mb-3">
   <label for="telefonoInput" class="form-label">Teléfono</label>
-  <input type="text" class="form-control" id="telefonoInput" name="telefono" value="{{ $cliente->telefono }}" required>
+  <input type="text" class="form-control {{ bsIsInvalidClass('telefono') }}" id="telefonoInput" name="telefono" value="{{ old('telefono', $cliente->telefono) }}" required>
+  <x-invalid-feedback name="telefono" />
 </div>
