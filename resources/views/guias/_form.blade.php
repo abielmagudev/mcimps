6e46df6a5414888e56a2252f5d808ab655f67dd9
Abiel Magu <abielmagu.dev@gmail.com>
2026-03-13 13:46:35 -0500

    reacomoda inputs del form de create/edit de guia

--- resources/views/guias/_form.blade.php
+++ resources/views/guias/_form.blade.php
@@ -1,14 +1,4 @@
 <div class="mb-3">
-  <label for="numeroRastreoUsaInput" class="form-label">Número de rastreo en USA {{ isset($guia->numero_rastreo_usa) ? '' : '(Requerido)' }}</label>
-  <input type="text" class="form-control {{ bsInputInvalid('numero_rastreo_usa') }}" id="numeroRastreoUsaInput" name="numero_rastreo_usa" value="{{ old('numero_rastreo_usa', $guia->numero_rastreo_usa) }}" @if(!$guia->exists) autofocus @endif required>
-  <x-invalid-feedback name="numero_rastreo_usa" />
-</div>
-<div class="mb-3">
-  <label for="numeroRastreoOrigenInput" class="form-label">Número de rastreo de origen (Opcional)</label>
-  <input type="text" class="form-control {{ bsInputInvalid('numero_rastreo_origen') }}" id="numeroRastreoOrigenInput" name="numero_rastreo_origen" value="{{ old('numero_rastreo_origen', $guia->numero_rastreo_origen) }}">
-  <x-invalid-feedback name="numero_rastreo_origen" />
-</div>
-<div class="mb-3">
   <label class="form-label" for="nombreClienteInput">Nombre del cliente (Opcional)</label>
     <input type="text" class="form-control {{ bsInputInvalid('nombre_cliente') }}" id="nombreClienteInput" name="nombre_cliente" value="{{ old('nombre_cliente', ($direccion?->prellenados['nombre_cliente'] ?? $guia->nombre_cliente) ) }}">
     <x-invalid-feedback name="nombre_cliente" />
@@ -17,6 +7,16 @@
   <label class="form-label" for="telefonoClienteInput">Teléfono del cliente (Opcional)</label>
   <input type="text" class="form-control {{ bsInputInvalid('telefono_cliente') }}" id="telefonoClienteInput" name="telefono_cliente" value="{{ old('telefono_cliente', ($direccion?->prellenados['telefono_cliente'] ?? $guia->telefono_cliente) ) }}">
   <x-invalid-feedback name="telefono_cliente" />
+</div>
+<div class="mb-3">
+  <label for="numeroRastreoUsaInput" class="form-label">Número de rastreo en USA {{ isset($guia->numero_rastreo_usa) ? '' : '(Requerido)' }}</label>
+  <input type="text" class="form-control {{ bsInputInvalid('numero_rastreo_usa') }}" id="numeroRastreoUsaInput" name="numero_rastreo_usa" value="{{ old('numero_rastreo_usa', $guia->numero_rastreo_usa) }}" @if(!$guia->exists) autofocus @endif required>
+  <x-invalid-feedback name="numero_rastreo_usa" />
+</div>
+<div class="mb-3">
+  <label for="numeroRastreoOrigenInput" class="form-label">Número de rastreo de origen (Opcional)</label>
+  <input type="text" class="form-control {{ bsInputInvalid('numero_rastreo_origen') }}" id="numeroRastreoOrigenInput" name="numero_rastreo_origen" value="{{ old('numero_rastreo_origen', $guia->numero_rastreo_origen) }}">
+  <x-invalid-feedback name="numero_rastreo_origen" />
 </div>
 <div class="mb-3">
   <label for="transportadoraAmericanaInput" class="form-label">Transportadora Americana</label>
