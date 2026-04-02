16f26db15c777a8e3b99a6459881c3cd19bb45a6
Abiel Magu <abielmagu.dev@gmail.com>
2026-04-02 04:04:59 -0500

    renombra columna de destino a direccion en index de guias

--- resources/views/guias/index.blade.php
+++ resources/views/guias/index.blade.php
@@ -12,7 +12,7 @@
         <x-slot name="thead">
             <tr>
                 <th></th>
-                <th style="min-width: 248px;">Destino</th>
+                <th style="min-width: 248px;">Dirección</th>
                 <th class="text-nowrap">Transportadora Americana</th>
                 <th class="text-nowrap">Transportadora Mexicana</th>
                 <th class="text-nowrap">Número de rastreo en USA</th>
